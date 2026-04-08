2b5e6ec1a0d2928c4f6be8d1571e1e2bb5be80a0
Kamokeo <theo.savouret@gmail.com>
2026-04-08 14:45:42 +0200

    move adsense script to <head> via beforeInteractive

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3230427510374994"
         crossOrigin="anonymous"
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
       />
     </html>
   )
